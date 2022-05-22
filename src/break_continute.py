25b85bca70f91d9b0cfc546733a1ad7b7984fd99
Tao Cao <tao.a.cao@gsk.com>
2022-05-22 07:43:00 +0100

    first commmit

---
+++ src/break_continute.py
@@ -0,0 +1,10 @@
+# use break and continue
+for i in range(6):
+    if i == 3:
+        break
+    print(i)
+# use continue to skip the rest of the loop
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
---
+++ src/hello_pytho.py
@@ -0,0 +1,9 @@
+#print hello world
+print("Hello World", end="")
+#print("Hello World")
+#print hello get input from keyboard
+name = input("What is your name? ")
+#print("Hello", name)
+print("Hello", name, "!")
+#print hello get input from keyboard    and add a number
+print("Hello", input("What is your name? "), "!")
